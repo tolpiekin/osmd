c6f0c5e6aef56bbcdbb6e15179ac7657b69ec4a7
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-04-19 23:49:15 +0300

    Initial commit. Spring Boot App.

---
+++ src/main/java/dev/tolpekin/vladimir/osmd/OsmdBillingProgramApplication.java
@@ -0,0 +1,13 @@
+package dev.tolpekin.vladimir.osmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmdBillingProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmdBillingProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tolpekin/vladimir/osmd/OsmdBillingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolpekin.vladimir.osmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmdBillingProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
